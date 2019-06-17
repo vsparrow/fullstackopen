b583d7426586a114839131b795a216ceb25991e5
vsparrow <sparrowangels@gmail.com>
2019-06-17 13:47:55 +0000

    compelte 1.9: unicafe step4 and step3

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -8,7 +8,35 @@ import ReactDOM from 'react-dom';
 //show stats
 const Heading = (props) => <h2>{props.title}</h2> 
 const Button = (props) => <button onClick={props.handleClick}>{props.name}</button>
-const Statistic = (props) => <div>{props.name} {props.value}</div>
+// const Statistics = (props) => <div>{props.name} {props.value}</div>
+const Statistics = (props) => {
+    let percent = ""
+    if(props.name === "positive"){percent = "%"}
+    // if(props.value === 0) {return(<div></div>)}
+    return(
+        <div>{props.name} {props.value}{percent}</div>        
+    )
+}
+const ShowStatistics = (props)=>{
+    if(props.good === 0 && props.neutral === 0 && props.bad === 0){
+        return <div>No feedback given</div>
+    }
+    let good = props.good
+    let neutral = props.neutral
+    let bad = props.bad
+    return(
+        <div>    
+            <Statistics name="good" value={good} />
+            <Statistics name="neutral" value={neutral} />
+            <Statistics name="bad" value={bad} />
+            <Statistics name="all" value={good + neutral + bad} />
+            <Statistics name="average" value={((good - bad) /(good + neutral + bad)) || 0} />
+            <Statistics name="positive" value={  (((good * 100) /(good + neutral + bad) )  || 0)} />
+        </div>    
+    )
+}
+
+
 const App = () => {
     //save clicks here
     const [good, setGood] = useState(0)
@@ -22,12 +50,7 @@ const App = () => {
             <Button handleClick={()=>{setNeutral(neutral + 1)}} name="neutral" />
             <Button handleClick={()=>{setBad(bad + 1)}} name="bad" />
             <Heading title="statistics"/>
-            <Statistic name="good" value={good} />
-            <Statistic name="neutral" value={neutral} />
-            <Statistic name="bad" value={bad} />
-            <Statistic name="all" value={good + neutral + bad} />
-            <Statistic name="average" value={((good - bad) /(good + neutral + bad)) || 0} />
-            <Statistic name="positive" value={  (((good * 100) /(good + neutral + bad) )  || 0) + "%"} />
+            <ShowStatistics good={good} neutral={neutral} bad={bad}/>
         </div>    
     )
 }
