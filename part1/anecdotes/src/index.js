bd62af7c764883931002a1a69aa097dacdadff87
vsparrow <sparrowangels@gmail.com>
2019-06-19 22:51:29 +0000

    complete 1.13*: anecdotes step2

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -11,15 +11,36 @@ const Button = (props)=>{
 
 const App = (props) => {
     const [selected, setSelected] = useState(0)
-    let random = Math.floor(Math.random() * props.anecdotes.length)    
+    let random = () => Math.floor(Math.random() * props.anecdotes.length)    
     const setToSelected = newValue => {setSelected(newValue)}
+    //create votes array and set each key to 0
+    const [votes,setVotes] = useState([0,0,0,0,0,0,0])
+    const setToVotes = (index) => {
+        const newVotes = [...votes]
+        newVotes[index] = newVotes[index] + 1
+        console.log(newVotes)
+        setVotes(newVotes)
+    }
+    // //votes as an object rather than array
+    // const [votesObj, setVotesObj] = useState({0:0,1:0,2:0,3:0,4:0,5:0})
+    // const setToVotesObj = (key) => {
+    //     const copy = { ...votesObj }
+    //     copy[selected] += 1
+    //     console.log(copy)
+    //     setVotesObj(copy)
+    // }
+    
     return (
         <div>
-            <p>{props.anecdotes[selected]}</p>
+            <div>{props.anecdotes[selected]}</div>
+            <div>has {votes[selected]} votes</div>
+            <Button  handleClick={()=>setToVotes(selected)} text="vote"/>
             <Button  handleClick={()=>{setToSelected(random)}} text="next anecdote" />
         </div>    
     )
 }
+
+
 
 const anecdotes = [
   'If it hurts, do it more often',
@@ -30,5 +51,5 @@ const anecdotes = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-ReactDOM.render(<App anecdotes={anecdotes}/>, document.getElementById('root'));
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
 
