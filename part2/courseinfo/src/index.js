0f305ab40f711bf0f48686039111e5d87d0adddc
vsparrow <khan@vsparrow.com>
2019-06-22 00:19:31 +0000

    complete 2.4: Course contents step9

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -68,30 +68,79 @@ const Course = (props) => {
 
 
 const App = () => {
-    const course = { 
-    name: "Half Stack application development",
-    parts: [
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
         {
-            name:  "Fundamentals of React",
-            exercises: 10
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
         },
         {
-            name:  "Using props to pass data",
-            exercises: 7
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
         },
         {
-            name:  "State of a component",
-            exercises: 14
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
         }
-    ]
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]    
+    // const course = { 
+    // name: "Half Stack application development",
+    // parts: [
+    //     {
+    //         name:  "Fundamentals of React",
+    //         exercises: 10
+    //     },
+    //     {
+    //         name:  "Using props to pass data",
+    //         exercises: 7
+    //     },
+    //     {
+    //         name:  "State of a component",
+    //         exercises: 14
+    //     }
+    // ]
+    // }
+    const allCourses = (courses)=>{
+        return courses.map(course => <Course course={course}/>)
     }
     return (
         <div>    
-            <Course course={course}/>
+            {allCourses(courses)}
         </div>    
     )
     
     //old return
+                // <Course course={course}/>
+    
             //     <Header course={course}/>
             // <Content course = {course} />
             // <Total course={course} />
