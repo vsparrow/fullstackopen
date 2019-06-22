181d8f06fac46d64bb88935c9726dc7b62a1a364
vsparrow <khan@vsparrow.com>
2019-06-22 00:07:08 +0000

    complete 2.2: Course contents step7

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -9,6 +9,9 @@ const Header = (props) => {
     )
 }
 
+
+
+
 const Part = (props) => {
     return (
         <p>
@@ -17,22 +20,33 @@ const Part = (props) => {
     )
 }
 
+const Total = (props) => {
+    const getTotal = (course) => {
+        let newsum = course.parts.reduce(function(accum, currentval){
+            console.log(currentval)
+            return accum + currentval.exercises
+        },0)
+        return newsum
+    }
+    return (
+        <div>
+            <p><strong>total of {getTotal(props.course)} exercises</strong></p>
+        </div>
+    )
+    // old return 
+    // <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+}
+
+
 const Content = (props) => {
     const allparts = (parts) => {
-        return parts.map(part => <Part part={part.name} exercises={part.exercises}/>)
+        return parts.map(part => <Part key={part.name} part={part.name} exercises={part.exercises}/>)
     }
     return (
         <div>
             {allparts(props.course.parts)}
+            <Total course={props.course}/>
         </div>    
-    )
-}
-
-const Total = (props) => {
-    return (
-        <div>
-            <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-        </div>
     )
 }
 
