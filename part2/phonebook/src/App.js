b1ef8d30e8ace9cda2880d105ddb15ddb08f6850
vsparrow <khan@vsparrow.com>
2019-06-23 05:20:27 +0000

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import React, {useState} from 'react'
 
 const App = (props) => {
-    const [persons, setPersons] = useState([{name: 'Arto Hellas', number: '040-1234567'}])
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [newFilter, setNewFilter] = useState('')
     
     const addPerson = (event) => {
         event.preventDefault()
@@ -20,9 +26,27 @@ const App = (props) => {
         }
     }
     
-    const showPersons = persons.map(person => 
+    const personsFiltered = persons.filter(person => {
+        // console.log(`is ${newFilter.toLowerCase()} in ${person.name}? ${person.name.toLowerCase().includes(newFilter.toLowerCase())}`)
+        return person.name.toLowerCase().includes(newFilter.toLowerCase())
+    })
+    
+    const personsToShow = newFilter.length === 0 ? persons : personsFiltered
+    // const personsToShow = newFilter.length === 0 ? persons : persons.filter(person => {
+    //     // console.log(`is ${newFilter.toLowerCase()} in ${person.name}? ${person.name.toLowerCase().includes(newFilter.toLowerCase())}`)
+    //     return person.name.toLowerCase().includes(newFilter.toLowerCase())
+    // })
+
+    const showPersons = personsToShow.map(person =>
         <div key={person.name}>{person.name} {person.number}</div>
     )
+    
+    //OLD
+    // const showPersons = persons.map(person => 
+    //     <div key={person.name}>{person.name} {person.number}</div>
+    // )
+    
+    
     
     const handleNameChange = (event) => {
         // console.log(event.target.value)
@@ -34,9 +58,16 @@ const App = (props) => {
         setNewNumber(event.target.value)
     }
     
+    const handleFilterChange = (event) => {
+        // console.log(event.target.value)
+        setNewFilter(event.target.value)
+    }
+    
     return (
         <div>
             <h2>Phonebook</h2>
+            <div>filter shown with<input onChange={handleFilterChange}/></div>
+            <h2>add a new</h2>
             <form onSubmit={addPerson}>
                 <div>name: <input value={newName} onChange={handleNameChange}/></div>
                 <div>number: <input onChange={handleNumberChange}/></div>
