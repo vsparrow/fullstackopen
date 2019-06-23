ddfd8f1589fd053d8903a1c9d4a5dc6546fea05f
vsparrow <khan@vsparrow.com>
2019-06-23 04:48:18 +0000

    2.8: The Phonebook Step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,27 @@
 import React, {useState} from 'react'
 
 const App = (props) => {
-    const [persons, setPersons] = useState([{name: 'Arto Hellas'}])
+    const [persons, setPersons] = useState([{name: 'Arto Hellas', number: '040-1234567'}])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
     
     const addPerson = (event) => {
         event.preventDefault()
         //prevent adding empty items to persons
-        if(newName.length > 0){
-            const newPersons = persons.concat({name: newName})
+        if(newName.length > 0 && newNumber.length > 0){
+            const newPersons = persons.concat({name: newName, number: newNumber})
             // console.log(newPersons)
             setPersons(newPersons)
-            setNewName('')            
+            setNewName('')
+            setNewNumber('')
         }
         else{
-            console.log("Please enter a name before pressing enter")
+            console.log("Please enter a name and number before pressing enter")
         }
     }
     
     const showPersons = persons.map(person => 
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>{person.name} {person.number}</div>
     )
     
     const handleNameChange = (event) => {
@@ -27,16 +29,18 @@ const App = (props) => {
         setNewName(event.target.value)
     }
     
+    const handleNumberChange = (event) => {
+        // console.log(event.target.value)
+        setNewNumber(event.target.value)
+    }
+    
     return (
         <div>
             <h2>Phonebook</h2>
             <form onSubmit={addPerson}>
-                <div>
-                    name: <input value={newName} onChange={handleNameChange}/>
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
+                <div>name: <input value={newName} onChange={handleNameChange}/></div>
+                <div>number: <input onChange={handleNumberChange}/></div>
+                <div><button type="submit">add</button></div>
             </form>
             <h2>Numbers</h2>
             
