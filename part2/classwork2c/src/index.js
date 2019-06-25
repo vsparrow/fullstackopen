0e830065a76dd4f39ff4d87c098dd1d011ab1ce8
vsparrow <khan@vsparrow.com>
2019-06-25 23:54:11 +0000

    classwork2c: setup json-server w/ package.json

---
+++ part2/classwork2c/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => {
+    return (
+        <div>    
+            Hi
+        </div>    
+    )
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
