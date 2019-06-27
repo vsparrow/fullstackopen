2f11e834dc74face8670f5172d15b733e9e26cda
vsparrow <khan@vsparrow.com>
2019-06-27 22:32:45 +0000

    dataforcountries: cleanup code

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -23,33 +23,7 @@ const App = (props) => {
     })
   }
   //DISPLAY COUNTRIES ////////////////////////////
-  
-  // const countryLanguages = (languages)=>
-  //   languages.map(language => <li key={language.name}>{language.name}</li>)
-  
-  // const displayCountries = () => {
-  //   // let displayData = <div></div>
-  //   if(countries.length === 0) {return <div></div>}
-  //   else if(countries.length > 10){return <div>Too many matches, specify another filter</div>}
-  //   else if(countries.length > 1) {
-  //     const toShow = countries.map(country=><div key={country.alpha3Code}>{country.name}</div>)
-  //     return toShow
-  //   }
-  //   else if(countries.length === 1){
-  //     return(
-  //       <div>
-  //         <h1>{countries[0].name}</h1>
-  //         <div>capital {countries[0].capital}</div>
-  //         <div>population {countries[0].population}</div>
-  //         <h2>languages</h2>
-  //         <ul>{countryLanguages(countries[0].languages)}</ul>
-  //         <img src={countries[0].flag} width="100px"/>
-  //       </div>
-  //     )
-  //   }
-  //   // return()
-  // }
-  //RETURN ///////////////////////////////////////
+
   return(
     <div>  
       find countries
--- part2/dataforcountries/src/components/Country.js
+++ part2/dataforcountries/src/components/Country.js
@@ -2,14 +2,9 @@ import React, {useState, useEffect} from "react";
 
 
 const Country = ({country}) => {
-    // console.log("inside component COUNTRY")
-    // console.log("length is ",country.length)
-    // console.log("country is ",country)
-    // console.log("************")
-    
+
     const countryLanguages = (languages)=>
         languages.map(language => <li key={language.name}>{language.name}</li>)
-
     
     if(!country){    return (<div></div>)}    
     const name = country.name ? country.name : "" 
@@ -27,27 +22,6 @@ const Country = ({country}) => {
         </div>    
         
     )
-    
-    
-
-    
-    // const countryLanguages = (languages)=>
-    //     languages.map(language => <li key={language.name}>{language.name}</li>)    
-    //  return(
-    //     <div>
-    //     </div>)
-
-    // return(
-    //     <div>
-    //         Country
-    //       <h1>{name}</h1>
-    //       <div>capital {country[0].capital}</div>
-    //       <div>population {country[0].population}</div>
-    //       <h2>languages</h2>
-    //       <ul>{countryLanguages(country[0].languages)}</ul>
-    //       <img src={country[0].flag} width="100px"/>          
-    //     </div>   
-    // )
 }
 
 export default Country
