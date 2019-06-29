8ad2e0da98d8c80d9b76d4db16553eff8e36476f
vsparrow <khan@vsparrow.com>
2019-06-29 04:53:44 +0000

    notes:add notes service worker

--- part2/classwork2c/src/App.js
+++ part2/classwork2c/src/App.js
@@ -1,18 +1,20 @@
 import React, {useState, useEffect} from 'react';
-import axios from 'axios'
+// import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = (props) => {
-    const jsonserverUrl = 'http://fullstackopen-vsparrow.c9users.io:8081/notes'
+    // const jsonserverUrl = 'http://fullstackopen-vsparrow.c9users.io:8081/notes'
     const [notes, setNotes] = useState([])
     const [newNote, setNewNote] = useState('')
     const [showAll, setShowAll] = useState(true)
     
     const hook = ()=>{
         // console.log('effect')
-        axios
-            .get(jsonserverUrl)
+        // axios
+        //     .get(jsonserverUrl)
             // .get('http://fullstackopen-vsparrow.c9users.io:8081/notes')
+        noteService.getAll()
             .then( response => {
                 // console.log('promise fulfilled')
                 setNotes(response.data)
@@ -25,12 +27,13 @@ const App = (props) => {
     // console.log('render',notes.length,'notes')
     const toggleImportanceOf = id => {
         // console.log('importance of ' + id + ' needs to be toggled')
-        const url = jsonserverUrl + '/' + id
+        // const url = jsonserverUrl + '/' + id
         // console.log(url)        
         const note = notes.find(n=>n.id === id)
         const changedNote = {...note, important: !note.important}
         
-        axios.put(url, changedNote)
+        // axios.put(url, changedNote)
+        noteService.update(id,changedNote)
         .then(response => {
             setNotes(notes.map(note => note.id !== id ? note : response.data))
         })
@@ -57,8 +60,9 @@ const App = (props) => {
             // id: notes.length + 1,               
         }
         
-        axios
-        .post(jsonserverUrl,noteObject)
+        noteService.create(noteObject)
+        // axios
+        // .post(jsonserverUrl,noteObject)
         .then(response => {
             // console.log(response)
             setNotes(notes.concat(response.data))
