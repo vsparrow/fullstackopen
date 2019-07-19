b00e4927356b74c13550873f9f487b838a2e8f51
khan@vsparrow <khan@vsparrow.com>
2019-07-19 00:20:05 +0000

    setup buildable notes frontend in part2 howework c

--- part2/classwork2c/src/services/notes.js
+++ part2/classwork2c/src/services/notes.js
@@ -1,10 +1,12 @@
-import axios from 'axios'
-const baseUrl = 'http://fullstackopen-vsparrow.c9users.io:8081/notes'
+import axios from 'axios';
+// const baseUrl = 'http://fullstackopen-vsparrow.c9users.io:8081/notes';
+// const baseUrl = 'https://infinite-basin-47509.herokuapp.com/notes/';
+const baseUrl = '/notes'; //this is when we * build * the react app and place it in the backend's folder
 
-const getAll = () => axios.get(baseUrl)
+const getAll = () => axios.get(baseUrl);
 
-const create = newObject => axios.post(baseUrl, newObject)
+const create = newObject => axios.post(baseUrl, newObject);
 
-const update = (id,newObject) => axios.put(`${baseUrl}/${id}`, newObject)
+const update = (id, newObject) => axios.put(`${baseUrl}/${id}`, newObject);
 
-export default {getAll, create, update}
+export default { getAll, create, update };
